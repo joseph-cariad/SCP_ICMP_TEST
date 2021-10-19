07289bb07b24cd44fb82b0e6ed3c1f3e28471672
Sindhya Shree R N <88767757+Sindhya-cariad@users.noreply.github.com>
2021-10-19 22:13:31 +0530

    Update TcpIpIcmpMsgHandler.c

--- TcpIpIcmpMsgHandler.c
+++ TcpIpIcmpMsgHandler.c
@@ -14,8 +14,14 @@ RemoteAddr.domain = TCPIP_AF_INET6;
 
 TcpIp_SockAddrType * const RemoteAddrPtr = (TcpIp_SockAddrType*)&RemoteAddr; //Type cast TcpIp_SockAddrInet6Type to TcpIp_SockAddrType
 
-TcpIp_IcmpMsgHandler(0,RemoteAddrPtr , 10 , 0x80 , 0x00 , 0 , NULL_PTR );     //Function to check if response is received.
-{
+TcpIp_IcmpMsgHandler(0,                                   //Local address identifier representing the local IP address        
+                     RemoteAddrPtr ,                      //pointer to struct representing the address of the ICMP sender
+                     10 ,                                 //Hop Limit value of the received ICMPv6 message.
+                     0x80 ,                               //type field value of the reveived ICMP message
+                     0x00 ,                               //code field value of the received ICMP message.
+                     0 ,                                  //length of ICMP message
+                     NULL_PTR                             //Pointer to the received ICMP message
+                     );    
     printf("Response is received\n");
     LED2();
 }
