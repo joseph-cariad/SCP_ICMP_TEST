2920d5984a07d7af861c5f63cb59ee4ecd7fab79
Sindhya Shree R N <88767757+Sindhya-cariad@users.noreply.github.com>
2021-10-19 21:57:12 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,39 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Tcpip_Api.h"
-#define IPV6ADDRMEMSIZE                16                 /*Length in Bytes for IPv6 Address */
-
-int RemoteAddress;
-unsigned char REMOTEADDRESS[][6] = {};                    //array to store ipv6 addresses
-
-TcpIp_SockAddrInet6Type RemoteAddr;                       //Variable containing IPv6 details
-
-MemCpy(RemoteAddr.addr, RemoteAddress, IPV6ADDRMEMSIZE);  //Assign RemoteAddr with Domain,Port,IPv6 address
-RemoteAddr.port = 0;
-RemoteAddr.domain = TCPIP_AF_INET6;
-
-
-TcpIp_SockAddrType * const RemoteAddrPtr = (TcpIp_SockAddrType*)&RemoteAddr; //Type cast TcpIp_SockAddrInet6Type to TcpIp_SockAddrType 
-
-void Up_IcmpMsgHandler();
 
 int main()
 {
     int button_status;
+    
     printf("Enter the value of button status:\n");
     scanf("%d", &button_status);
+    
     if(button_status == 1)
         button_status++;
         TcpIp_IcmpTransmit ( 0,RemoteAddrPtr , 10 , 0x80 , 0x00 , 0 , NULL_PTR );
 
-return 0;
+    return 0;
 }
-
-TcpIp_IcmpMsgHandler(0,RemoteAddrPtr , 10 , 0x80 , 0x00 , 0 , NULL_PTR );     //Function to check if response is received.
-{
-    printf("Response is received\n");
-    LED2();
-}
-
-
-
