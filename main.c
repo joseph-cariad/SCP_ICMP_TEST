aa486fd567f998e9e8ce9328610dca3c0200a383
Sindhya Shree R N <88767757+Sindhya-cariad@users.noreply.github.com>
2021-10-19 16:48:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,20 @@
 #include "Tcpip_Api.h"
 #define IPV6ADDRMEMSIZE                16                 /*Length in Bytes for IPv6 Address */
 
-int i, RemoteAddress, Button1_status, count;
-unsigned char REMOTEADDRESS[][i] = {};
+int RemoteAddress;
+unsigned char REMOTEADDRESS[][6] = {};                    //array to store ipv6 addresses
 
 TcpIp_SockAddrInet6Type RemoteAddr;                       //Variable containing IPv6 details
 
-MemCpy(RemoteAddr.addr, RemoteAddress, IPV6ADDRMEMSIZE);  //
+MemCpy(RemoteAddr.addr, RemoteAddress, IPV6ADDRMEMSIZE);  //Assign RemoteAddr with Domain,Port,IPv6 address
 RemoteAddr.port = 0;
 RemoteAddr.domain = TCPIP_AF_INET6;
 
 
-TcpIp_SockAddrType * const RemoteAddrPtr = (TcpIp_SockAddrType*)&RemoteAddr;
+TcpIp_SockAddrType * const RemoteAddrPtr = (TcpIp_SockAddrType*)&RemoteAddr; //Type cast TcpIp_SockAddrInet6Type to TcpIp_SockAddrType 
 
 void Up_IcmpMsgHandler();
+
 int main()
 {
     int button_status;
@@ -28,7 +29,7 @@ int main()
 return 0;
 }
 
-TcpIp_IcmpMsgHandler(0,RemoteAddrPtr , 10 , 0x80 , 0x00 , 0 , NULL_PTR )
+TcpIp_IcmpMsgHandler(0,RemoteAddrPtr , 10 , 0x80 , 0x00 , 0 , NULL_PTR );     //Function to check if response is received.
 {
     printf("Response is received\n");
     LED2();
