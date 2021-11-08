ac7266456825558aa064c64ae75a6c06bbd1cfae
lokesh-cariad <88821504+lokesh-cariad@users.noreply.github.com>
2021-11-08 10:07:07 +0530

    Add files via upload
    LED names and channel id changed.

--- led_flash_handler.c
+++ led_flash_handler.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <dio.h>      
+     
 #include <led_flash_handler.h>
 /*
-led_1_handler function is used to turn on/off user led 1(151).
-input parameter: bool turn_on   : true to turn ON led 1
-                                : false to turn OFF led 1
+led_1_handler function is used to turn on/off user led 0(150).
+input parameter: bool turn_on   : true to turn ON led 0
+                                : false to turn OFF led 0
 return type: None
 */
-void led_1_handler (bool turn_on)
+void led_0_handler (bool turn_on)
 {
     if (turn_on){
-        Dio_WriteChannel(151, STD_HIGH);
+        Dio_WriteChannel(150, STD_HIGH);
     }else{
-        Dio_WriteChannel(151, STD_LOW);
+        Dio_WriteChannel(150, STD_LOW);
     }
 }
 
 
 /*
-led_2_handler function is used to turn on/off user led 2 (152).
-input parameter: bool turn_on   : true to turn ON led 2
-                                : false to turn OFF led 2
+led_2_handler function is used to turn on/off user led 1 (152).
+input parameter: bool turn_on   : true to turn ON led 1
+                                : false to turn OFF led 1
 return type: None
 */
-void led_2_handler (bool turn_on)
+void led_1_handler (bool turn_on)
 {
     if (turn_on){
         Dio_WriteChannel(152, STD_HIGH);
@@ -35,10 +35,10 @@ void led_2_handler (bool turn_on)
 
 
 /*
-led_4_5_6_handler function is used to turn on/off user led 4, 5 & 6.
+led_4_5_6_handler function is used to turn on/off user led 3, 4 & 5.
 
 
-                                        Value       led 4(154)  led 5(282)  led 6(283)
+                                        Value       led 3(154)  led 4(281)  led 5(282)
  input parameter: uint8_t binary_code:  1           OFF         OFF         ON          //ZeC
                                         2           OFF         ON          OFF         //VCP
                                         3           OFF         ON          ON          //ADAS
@@ -48,26 +48,26 @@ led_4_5_6_handler function is used to turn on/off user led 4, 5 & 6.
 return type: None
 
 */
-void led_4_5_6_handler(uint8_t binary_code)
+void led_3_4_5_handler(uint8_t binary_code)
 {
     if (binary_code%2){
-        Dio_WriteChannel(283, STD_HIGH); //Led 6(ON)
-    }else{
-        Dio_WriteChannel(283, STD_LOW);//Led 6(OFF)
-    }
-
-    binary_code=binary_code/2;
-    if (binary_code%2){
-        Dio_WriteChannel(282, STD_HIGH);//Led 5(ON)
+        Dio_WriteChannel(282, STD_HIGH); //Led 5(ON)
     }else{
         Dio_WriteChannel(282, STD_LOW);//Led 5(OFF)
     }
 
     binary_code=binary_code/2;
     if (binary_code%2){
-        Dio_WriteChannel(154, STD_HIGH);//Led 4(ON)
+        Dio_WriteChannel(281, STD_HIGH);//Led 4(ON)
     }else{
-        Dio_WriteChannel(154, STD_LOW);//Led 4(OFF)
+        Dio_WriteChannel(281, STD_LOW);//Led 4(OFF)
+    }
+
+    binary_code=binary_code/2;
+    if (binary_code%2){
+        Dio_WriteChannel(154, STD_HIGH);//Led 3(ON)
+    }else{
+        Dio_WriteChannel(154, STD_LOW);//Led 3(OFF)
     }
 }
 
--- led_flash_handler.h
+++ led_flash_handler.h
@@ -5,10 +5,10 @@
 #include <stdbool.h>
 #include <dio.h>   
 
-extern void led_1_handler (bool turn_on);
+extern void led_0_handler (bool turn_on); //led_0_handler function is used to turn on/off user led 0(150)
 
-extern void led_2_handler (bool turn_on);
+extern void led_1_handler (bool turn_on); //led_1_handler function is used to turn on/off user led 1(152).
 
-extern void led_4_5_6_handler(uint8_t binary_code);
+extern void led_3_4_5_handler(uint8_t binary_code); //led_3_4_5_handler function is used to turn on/off user led 3, 4 & 5.
 
 #endif // _LED_FLASH_HANDLER_H_
